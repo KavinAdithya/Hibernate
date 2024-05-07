d5a6193204bab2b8f1c75e5d1a1c2a962662f1c6
KaVin S <kavinadithya3@gmail.com>
2024-05-07 10:43:18 +0530

    Code Organised

--- src/main/java/com/techcrack/Hibernate/techcrack/App.java
+++ src/main/java/com/techcrack/Hibernate/techcrack/App.java
@@ -29,61 +29,14 @@ public class App {
     	SessionFactory sf=config.buildSessionFactory();
     	
     	Session session=sf.openSession();
-//    	
-//    	Professor wif=new Professor();
-//    	
-//    	wif.setName("Vijay");
-//    	wif.setAge(54);
-//    	wif.setId(488);
-//    	wif.setSpouse("Samantha");
-//    	wif.setDegree("BE.ME");
-//    	wif.setChild("Joseph Vijay");
-//    	wif.setPhoneNumber("7456981527");
-//    	wif.setEmail("vijaytvk@gmail.com");
-//    	wif.setFather("Kuruvi");
-//    	
+   	
     	Transaction trans=session.beginTransaction();
-//    	
-//    	Name name=new Name();
-//    	name.setLeftName("Computer");
-//    	name.setMiddleName("Science");
-//    	name.setRightName("Engineering");
-//    	
-//    	Department depart=new Department();
-//    	depart.setId(1);
-//    	depart.setName(name);
-//    	
-//    	Name ece=new Name();
-//    	ece.setLeftName("Electronics");
-//    	ece.setMiddleName(" And Communication");
-//    	ece.setRightName("Enginaeering");
-//    	
-//    	Department d2=new Department();
-//    	d2.setId(2);
-//    	d2.setName(ece);
-//    	
-//    	List<Department> list=new ArrayList<>();
-//    	list.add(depart);
-//    	list.add(d2);
-//    	//session.persist(d2);
-//    	//session.persist(depart);
-//    	wif.setDepart_id(list);
-//    	session.persist(wif);
-    	//session.persist(wif);
-    	//System.out.println(session.get(Professor.class, 19));
+
+    	//Data Insert
+    	//dataInsert(session);
     	
-//    	Professor ramya=session.get(Professor.class,1);
-//    	
-//    	ramya.setFather("John");
-//    	ramya.setDegree("BE.ME");
-//    	
-//    	session.update(ramya);
-    	
-//    	Professor saranya=session.get(Professor.class,99);
-//    	
-//    	session.delete(saranya);
-    	
-    	eagerAndLazy(session);
+    	//Eager
+    	//eagerAndLazy(session);
     	trans.commit();
     }
     
@@ -97,6 +50,63 @@ public class App {
     		System.out.println(d);
     	}
     	
+    }
+    
+    
+    //Method whic is used to insert data on database both mapping relationship[ as well as
+    private static void dataInsert(Session session) {
+    	
+    	Professor wif=new Professor();
+    	
+    	wif.setName("Vijay");
+    	wif.setAge(54);
+    	wif.setId(488);
+    	wif.setSpouse("Samantha");
+    	wif.setDegree("BE.ME");
+    	wif.setChild("Joseph Vijay");
+    	wif.setPhoneNumber("7456981527");
+    	wif.setEmail("vijaytvk@gmail.com");
+    	wif.setFather("Kuruvi");
+    	
+    	
+    	
+    	Name name=new Name();
+    	name.setLeftName("Computer");
+    	name.setMiddleName("Science");
+    	name.setRightName("Engineering");
+    	
+    	Department depart=new Department();
+    	depart.setId(1);
+    	depart.setName(name);
+    	
+    	Name ece=new Name();
+    	ece.setLeftName("Electronics");
+    	ece.setMiddleName(" And Communication");
+    	ece.setRightName("Enginaeering");
+    	
+    	Department d2=new Department();
+    	d2.setId(2);
+    	d2.setName(ece);
+    	
+    	List<Department> list=new ArrayList<>();
+    	list.add(depart);
+    	list.add(d2);
+    	session.persist(d2);
+    	session.persist(depart);
+    	wif.setDepart_id(list);
+    	session.persist(wif);
+    	session.persist(wif);
+    	System.out.println(session.get(Professor.class, 19));
+    	
+    	Professor ramya=session.get(Professor.class,1);
+    	
+    	ramya.setFather("John");
+    	ramya.setDegree("BE.ME");
+    	session.update(ramya);
+    	
+    	Professor saranya=session.get(Professor.class,99);
+    	
+    	session.delete(saranya);
     	
     }
 }
